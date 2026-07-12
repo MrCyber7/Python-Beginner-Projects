c626a9b30c069c355b2ae7358f53c9d349486173
Zyphore <smaz152009@gmail.com>
2026-07-12 20:18:16 +0500

    Create even_number_printer.py

---
+++ even_number_printer.py
@@ -0,0 +1,11 @@
+# Even number printer
+
+num = int(input("Enter: "))
+
+while num < 0:
+  num = int(input("Enter: "))
+for nums in range(1, num + 1):
+  if nums % 2 == 0:
+    print(nums)
+  else:
+    continue
