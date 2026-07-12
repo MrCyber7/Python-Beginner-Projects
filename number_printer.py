64d6e1597adac4a2bfa33e078b720c8c30233556
Zyphore <smaz152009@gmail.com>
2026-07-12 20:12:29 +0500

    Create number_printer.py

---
+++ number_printer.py
@@ -0,0 +1,13 @@
+# Number printer
+
+num = int(input("Enter: "))
+
+for x in range(1, num+1):
+  if x % 3 == 0 and x % 5 == 0:
+      print("FizzBuzz")
+  elif x % 5 == 0:
+    print("Buzz")
+  elif x % 3 == 0:
+    print("Fizz")
+  else:
+    print(x)
