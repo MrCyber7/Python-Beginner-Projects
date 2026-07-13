e4c6e6a3890b4783a7048b37d603a77a7df6eb3b
Zyphore <smaz152009@gmail.com>
2026-07-13 15:08:41 +0500

    Create number_pyramid.py

---
+++ number_pyramid.py
@@ -0,0 +1,16 @@
+# Number Pyramid
+
+row = int(input("Enter Rows: "))
+
+while row <= 0:
+  print("Negative Error!")
+  row = int(input("Enter Rows: "))
+
+for rows in range(1, row + 1):
+  for spaces in range(row - rows):
+    print(" " , end="")
+  for column in range(1, rows + 1):
+    print(column , end="")
+  for opposite in reversed(range(1, column)):
+    print(opposite, end="")
+  print()
